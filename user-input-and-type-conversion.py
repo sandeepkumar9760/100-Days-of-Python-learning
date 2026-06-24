be27e329a51a47e821d1323f842cb972d8bbb212
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-06-24 13:01:51 +0530

    new file

---
+++ user-input-and-type-conversion.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+print(name)
+
+first_num = input("Enter your first num: ")
+second_num = input("Enter your second number: ")
+print(first_num + second_num)
+
+print("You will see numbers are not added mathematically , because python by default takes it as string so we need to specify the data type of the input we want from the user.")
+
+first_num = int(input("Enter the number 1: "))
+second_num = int(input("Enter the number 2: "))
+
+print(first_num+second_num)
+
+print("Now the numbers are added , because now we have specified the data types of the input , and if user gives string as a input it will throw an error.")
