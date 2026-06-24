a8eb514ea91c90535bb5d54428998781072d2e3e
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-06-24 20:22:09 +0530

    if else file

---
+++ If-else.py
@@ -0,0 +1,16 @@
+#if else 
+
+email = "sandeepkumar@gmail.com"
+password = '123456789'
+
+user_email = input("enter your email: ")
+user_pass = input("enter your password: ")
+
+if user_email == email and user_pass==password:
+    print("Welcome")
+else:
+    print("wrong credentials")
+
+
+#if elif .... else
+# we use elif for multiple conditions 
